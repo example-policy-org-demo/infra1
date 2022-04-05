e972b2a59475b5041c59304ad2432a6acf213ab6
Chris Nesbitt-Smith <chris@cns.me.uk>
2022-04-05 15:43:58 +0100

    update tf

--- main.tf
+++ main.tf
@@ -1,6 +1,12 @@
-resource "aws_s3_bucket" "b" {
-  bucket = "my-tf-test-bucket"
-  tags = {
-    mycompany.com.department = "finance"
-  }
+resource "aws_db_instance" "default" {
+  allocated_storage    = 10
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t3.micro"
+  name                 = "mydb"
+  username             = "foo"
+  password             = "foobarbaz"
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
+  storage_encrypted    = true
 }
